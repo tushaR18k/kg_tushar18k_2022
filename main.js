661c33c25ae5313dddb40077f0f5916ef22a32b4
Tushar <kansaltushar18@gmail.com>
2021-05-03 11:57:05 -0400

    Added for loops for processing of inputs and converting the numbers to their word representations

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
+const numberMappings = {
+    '0':'Zero',
+    '1': 'One',
+    '2': 'Two',
+    '3':'Three',
+    '4':'Four',
+    '5':'Five',
+    '6':'Six',
+    '7':'Seven',
+    '8':'Eight',
+    '9':'Nine'
+}
+
 let myargs = process.argv.slice(2)
-console.log('my args: ',myargs)
+// console.log('my args: ',myargs)
+myargs.forEach(element => {
+    let stringnum = ""
+    for(let i=0;i<element.length;i++){
+        stringnum += numberMappings[element[i]]
+    }
+    console.log(stringnum)
+});
